e1c8069b3f652b88496d1044a5cc835c659ea25a
timoun-sayang <thomasho7718@gmail.com>
2026-03-15 08:06:44 +0100

    je change pygame en turtle, moins complexe

--- snake.py
+++ snake.py
@@ -1,21 +1,9 @@
-import pygame, sys
+import turtle
 
-pygame.init()
-screen = pygame.display.set_mode((400,500))
-clock = pygame.time.Clock()
-test_surface = pygame.Surface((100,200))
-test_surface.fill(pygame.Color(0,0,255))
-test_rectangle = pygame.Rect(100,200,100,100)
+screenGUI = turtle.Screen()
+screenGUI.title("Jeu Snake par Thomas")
+screenGUI.bgcolor("green")
+screenGUI.setup(width=600,height= 600)
+screenGUI.tracer(0)
 
-
-while True:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
-            sys.exit()
-    screen.fill(pygame.Color(175,215,70))
-    screen.blit(test_surface,(150,250))
-    pygame.draw.rect(screen,pygame.Color('red'), test_rectangle)
-    pygame.display.update()
-    clock.tick(30)
-    
+screenGUI.mainloop()
