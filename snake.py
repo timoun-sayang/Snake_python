194e3ad5ab5d3aaa36e8e0cb028a90dc3118b3c0
timoun-sayang <thomasho7718@gmail.com>
2026-03-15 07:48:59 +0100

    Initialisation du jeu snake

--- snake.py
+++ snake.py
@@ -1,1 +1,21 @@
-print("coucou")
+import pygame, sys
+
+pygame.init()
+screen = pygame.display.set_mode((400,500))
+clock = pygame.time.Clock()
+test_surface = pygame.Surface((100,200))
+test_surface.fill(pygame.Color(0,0,255))
+test_rectangle = pygame.Rect(100,200,100,100)
+
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+    screen.fill(pygame.Color(175,215,70))
+    screen.blit(test_surface,(150,250))
+    pygame.draw.rect(screen,pygame.Color('red'), test_rectangle)
+    pygame.display.update()
+    clock.tick(30)
+    
